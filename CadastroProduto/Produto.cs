1a2759f52df4c6ba043286d0e4557be03de0a1a5
AlefMelo <alefmelo13@gmail.com>
2023-02-10 17:43:52 -0300

    refurmulacao código

--- CadastroProduto/Produto.cs
+++ CadastroProduto/Produto.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace CadastroProduto
 {
-    class Produto
+    public class Produto
     {
         public int Id { get; set; }
         public string Nome { get; set; }
@@ -24,6 +24,74 @@ namespace CadastroProduto
         {
             Nome = nome;
             Preco = preco;
+        }
+
+        public static void IncluirProduto()
+        {
+            Console.Clear();
+            Console.Write("Quantos produtos deseja incluir? ");
+            int quantidade = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            var produto = new List<Produto>();
+
+            for (int i = 1; i <= quantidade; i++)
+            {
+                Console.Clear();
+                Console.WriteLine($"Cadastre o Produto {i}");
+                Console.WriteLine("-----------------------");
+                Console.Write("Código do Produto: ");
+                int codigoProduto = int.Parse(Console.ReadLine());
+                Console.Write("Nome do Produto: ");
+                string nomeProduto = Console.ReadLine();
+                Console.Write("Preço do Produto: ");
+                decimal precoProduto = decimal.Parse(Console.ReadLine());
+
+                produto.Add(new Produto(codigoProduto, nomeProduto, precoProduto));
+
+                EditarProduto(produto);
+
+                Console.Clear();
+                Console.WriteLine("Produto Cadastrado com Sucesso!");
+                Thread.Sleep(2000);
+            }
+
+            Thread.Sleep(2000);
+
+            Program.Menu(produto);
+        }
+
+        public static void EditarProduto(List<Produto> produto)
+        {
+            Console.Clear();
+
+            Console.Write("Digite o código do produto a ser editado: ");
+            int codigoProduto = int.Parse(Console.ReadLine());
+
+            foreach (Produto prod in produto)
+            {
+                if (prod.Id == codigoProduto)
+                {
+                    Console.WriteLine("Dados do Produto");
+                    Console.WriteLine("----------------");
+                    Console.WriteLine($"Código do Produto: {prod.Id}");
+                    Console.WriteLine($"Nome do Produto: {prod.Nome}");
+                    Console.WriteLine($"Preço do Produto: {prod.Preco}");
+                    Console.WriteLine("------------------");
+                    Console.WriteLine();
+
+                    Console.Write("Novo Nome: ");
+                    string novoNome = Console.ReadLine();
+                    Console.Write("Novo Preço: ");
+                    decimal novoPreco = decimal.Parse(Console.ReadLine());
+
+                    produto.Add(new Produto(novoNome, novoPreco));
+                }
+                else
+                {
+                    Console.WriteLine("Produto não localizado!");
+                }
+            }
         }
     }
 }
--- CadastroProduto/Program.cs
+++ CadastroProduto/Program.cs
@@ -1,13 +1,15 @@
-﻿namespace CadastroProduto
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CadastroProduto
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Menu();
         }
 
-        static void Menu()
+        public static void Menu(List<Produto> produro)
         {
             Console.Clear();
             Console.WriteLine("Cadastro de Produto");
@@ -24,8 +26,8 @@
 
             switch(resposta)
             {
-                case 1: IncluirProduto(); break;
-                case 2: EditarProduto(); break;
+                case 1: Produto.IncluirProduto(); break;
+                case 2: Produto.EditarProduto(produro); ; break;
                 case 3: ConsultarProduto(); break;
                 case 4: ExcluirProduto(); break;
                 case 0: Console.Clear();
@@ -35,79 +37,12 @@
             }
         }
 
-        static void IncluirProduto()
-        {
-            Console.Clear();
-            Console.Write("Quantos produtos deseja incluir? ");
-            int quantidade = int.Parse(Console.ReadLine());
-            Console.Clear();
-
-            var produto = new List<Produto>();
-
-            for (int i = 1; i <= quantidade; i++)
-            {
-                Console.Clear();
-                Console.WriteLine($"Cadastre o Produto {i}");
-                Console.WriteLine("-----------------------");
-                Console.Write("Código do Produto: ");
-                int codigoProduto = int.Parse(Console.ReadLine());
-                Console.Write("Nome do Produto: ");
-                string nomeProduto = Console.ReadLine();
-                Console.Write("Preço do Produto: ");
-                decimal precoProduto = decimal.Parse(Console.ReadLine());
-
-                produto.Add(new Produto(codigoProduto, nomeProduto, precoProduto));
-
-
-                Console.Clear();
-                Console.WriteLine("Produto Cadastrado com Sucesso!");
-                Thread.Sleep(2000);
-            }
-
-            Thread.Sleep(2000);
-
-            Menu();
-        }
-
-        static void EditarProduto()
-        {
-            var produto = new List<Produto>();
-
-            Console.Write("Digite o código do produto a ser editado: ");
-            int codigoProduto = int.Parse(Console.ReadLine());
-
-            foreach (Produto prod in produto)
-            {
-                if(prod.Id == codigoProduto)
-                {
-                    Console.WriteLine("Dados do Produto");
-                    Console.WriteLine("----------------");
-                    Console.WriteLine($"Código do Produto: {prod.Id}");
-                    Console.WriteLine($"Nome do Produto: {prod.Nome}");
-                    Console.WriteLine($"Preço do Produto: {prod.Preco}");
-                    Console.WriteLine("------------------");
-                    Console.WriteLine();
-
-                    Console.Write("Novo Nome: ");
-                    string novoNome = Console.ReadLine();
-                    Console.Write("Novo Preço: ");
-                    decimal novoPreco = decimal.Parse(Console.ReadLine());
-
-                    produto.Add(new Produto(novoNome, novoPreco));
-                }
-                else
-                {
-                    Console.WriteLine("Produto não localizado!");
-                }
-            }
-        }
-
-        static void ExcluirProduto()
+        public static void ExcluirProduto()
         {
             Console.WriteLine();
         }
 
-        static void ConsultarProduto()
+        public static void ConsultarProduto()
         {
             Console.WriteLine();
         }
